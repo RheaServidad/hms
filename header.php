a259ba5466fb7b88c8c34c44750bacff9f2ef73d
RheaServidad <145845164+RheaServidad@users.noreply.github.com>
2024-02-01 12:01:50 +0800

    Update header.php

--- header.php
+++ header.php
@@ -6,21 +6,12 @@ include 'conn.php';
 <!DOCTYPE html>
 <html lang="en">
 <style>
-    .icon {
-        grid-row: 3/4;
-        grid-column: 4/5;
-    }
+     .icon {
 
-    .rhea {
-        margin-left: 81%;
-    }
-
-    .avatar {
-        width: 100%;
-        height: 10%;
-    }
+grid-row: 3/4;
+grid-column: 4/5;
+}
 </style>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -32,57 +23,32 @@ include 'conn.php';
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="img/m.jpg" style="border-radius: 50%;" width="70" height="70"
-                    alt=""></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <div class="icon">
+            <div class="icon">
+                <img src="img/m.jpg" width="70" height="70"> </div>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="client.php">Clients</a>
+                        <a class="nav-link" href="index.php?view&id">Clients</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.php?edit&id">About</a>
+                        <a class="nav-link" href="index.php?edit&id">About</a>
                     </li>
                 </ul>
-                <ul class="navbar-nav rhea"> <!-- Added ms-auto class to move the ul to the right -->
+                <ul class="navbar-nav">
                     <li class="nav-item">
                         <?php
                         if (isset($_SESSION['logged_in'])) { ?>
-                            <div class="dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" id="profileDropdown"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    <div class="avatar" align="right">
-                                        <img src="img/pIcon.jpg" height="100%" width="100%" style="border-radius: 50%;">
-                                    </div>
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="profileDropdown">
-                                    <li>
-                                        <a class="dropdown-item" href="profile.php?profile&id=<?= $_SESSION['u_id'] ?>">View
-                                            Profile</a>
-                                    </li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="process.php?logout">Logout</a>
-                                    </li>
-                                </ul>
-                            </div>
+                            <a href="process.php?logout" class="nav-link">Logout</a>
                         <?php } else { ?>
                             <a href="login.php" class="nav-link">Login</a>
                         <?php } ?>
                     </li>
                 </ul>
+
             </div>
         </div>
     </nav>
-</body>
-
-</html>
